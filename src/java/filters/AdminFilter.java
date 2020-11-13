c57c079d221ae8797c71bcf53d28902cbdddbb19
775653 <775653@SAIT224373IT.ACDM.DS.SAIT.CA>
2020-11-13 09:50:14 -0700

    Implemented Admin filter Added admin filter to web xml. Coded admin filter to only allow admins through

---
+++ src/java/filters/AdminFilter.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package filters;
+
+import dataaccess.UserDB;
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import models.User;
+
+/**
+ *
+ * @author 775653
+ */
+public class AdminFilter implements Filter {
+
+    public AdminFilter() {
+        
+    }    
+   
+    public void doFilter(ServletRequest request, ServletResponse response,
+            FilterChain chain)
+            throws IOException, ServletException {
+        
+            HttpServletRequest httpRequest = (HttpServletRequest) request;
+            HttpServletResponse httpResponse = (HttpServletResponse) response;
+            
+            HttpSession session = httpRequest.getSession();
+            
+            String email = (String) session.getAttribute("email");
+            UserDB userDB = new UserDB();
+            
+            User user = userDB.get(email);
+            
+            if(user.getRole().getRoleId() != 1){
+                httpResponse.sendRedirect("notes");
+                return;
+            }
+     
+            chain.doFilter(request, response);
+        
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        
+    }
+
+    @Override
+    public void destroy() {
+    
+    }
+}
